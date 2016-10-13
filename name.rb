c60e8eb52f1f83eea5d622e7e8e0d129c0d40210
Rob Donigian <rpdonigian@gmail.com>
2016-10-12 23:02:04 -0400

    I added the Variable exercises.

---
+++ name.rb
@@ -0,0 +1,11 @@
+puts "Please enter your first name."
+f_name = gets.chomp.capitalize!
+puts "Please enter your last name."
+l_name = gets.chomp.capitalize!
+puts "Your full name is " + f_name +" "+ l_name + "."
+
+=begin
+10.times do |n|
+  puts name
+end 
+=end
